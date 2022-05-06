b23e07bdd84bcc0aed0f4ceb92c2359bfc625574
Rockheung <rockheung@gmail.com>
2022-05-06 21:53:55 +0900

    update setting

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,40 @@
-var gulp = require('gulp');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var tsify = require('tsify');
+var gulp = require("gulp");
+var browserify = require("browserify");
+var source = require("vinyl-source-stream");
+var tsify = require("tsify");
+var uglify = require("gulp-uglify");
+var sourcemaps = require("gulp-sourcemaps");
+var buffer = require("vinyl-buffer");
 var paths = {
-    pages: ['src/*.html']
+  pages: ["src/*.html"],
 };
-gulp.task('copy-html', function () {
-    return gulp.src(paths.pages)
-        .pipe(gulp.dest('dist'));
+
+gulp.task("copy-html", function () {
+  return gulp.src(paths.pages).pipe(gulp.dest("dist"));
 });
-gulp.task('default', gulp.series(gulp.parallel('copy-html'), function () {
+
+gulp.task(
+  "default",
+  gulp.series(gulp.parallel("copy-html"), function () {
     return browserify({
-        basedir: '.',
-        debug: true,
-        entries: ['src/amondz.ts'],
-        cache: {},
-        packageCache: {}
+      basedir: ".",
+      debug: true,
+      entries: ["src/amondz.ts"],
+      cache: {},
+      packageCache: {},
     })
-    .plugin(tsify)
-    .bundle()
-    .pipe(source('amondz.js'))
-    .pipe(gulp.dest('dist'));
-}));
+      .plugin(tsify, { target: "es6" })
+      .transform("babelify", {
+        presets: ["@babel/preset-env"],
+        plugins: ["@babel/plugin-transform-runtime"],
+        extensions: [".tsx", ".ts"],
+      })
+      .bundle()
+      .pipe(source("amondz.js"))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(uglify())
+      .pipe(sourcemaps.write("./"))
+      .pipe(gulp.dest("dist"));
+  })
+);
--- src/amondz.ts
+++ src/amondz.ts
@@ -1,5 +1,78 @@
+import Cookies from "js-cookie";
+
+type InfoPayload = {
+  storeId: number;
+};
+
+type ListPayload = {
+  exceptAllCount: boolean;
+  startIndex: number;
+};
+
+const getCompanyInfo = (body: InfoPayload) =>
+  fetch("https://api.amondz.com/api/brand/detail/info/web/v1", {
+    headers: {
+      accept: "application/json, text/plain, */*",
+      "accept-language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7,la;q=0.6",
+      "cache-control": "no-cache",
+      "content-type": "application/json;charset=UTF-8",
+      "device-type": "2",
+      os: "Mac",
+      "page-view": "9",
+      pragma: "no-cache",
+      "sec-ch-ua":
+        '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": '"macOS"',
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-site",
+      "user-key": "null",
+      uuid: Cookies.get("amondz_uuid"),
+    } as HeadersInit,
+    referrer: "https://www.amondz.com/",
+    referrerPolicy: "strict-origin-when-cross-origin",
+    method: "POST",
+    mode: "cors",
+    credentials: "include",
+    body: JSON.stringify(body),
+  });
+
+const getCompanyList = (body: ListPayload) =>
+  fetch("https://api.amondz.com/api/brand/list/pagination/web/v1", {
+    headers: {
+      accept: "application/json, text/plain, */*",
+      "accept-language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7,la;q=0.6",
+      "cache-control": "no-cache",
+      "content-type": "application/json;charset=UTF-8",
+      "device-type": "2",
+      os: "Mac",
+      "page-view": "4",
+      pragma: "no-cache",
+      "sec-ch-ua":
+        '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": '"macOS"',
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-site",
+      "user-key": "null",
+      uuid: Cookies.get("amondz_uuid"),
+    } as HeadersInit,
+    referrer: "https://www.amondz.com/",
+    referrerPolicy: "strict-origin-when-cross-origin",
+    method: "POST",
+    mode: "cors",
+    credentials: "include",
+    body: JSON.stringify(body),
+  });
+
 async function main() {
-  console.log(Date.now());
+  let _csv = "";
+  throw await getCompanyList({
+    startIndex: 0,
+    exceptAllCount: true,
+  });
 }
 
 main().catch(console.error);
